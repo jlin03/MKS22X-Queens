2e8b191f231d6a91036d288d1b344878d649655d
jlin03 <jlin03@stuy.edu>
2019-02-07 11:35:56 -0500

    markSpots method written, not tested

--- QueenBoard.java
+++ QueenBoard.java
@@ -5,7 +5,17 @@ public class QueenBoard {
     board = new int[size][size];
   }
 
-
+  private void markSpots(int r, int c, int shift) {
+    for(int s = 1;r+s >= 0 && r+s < board.length && c+s >= 0 && c+s < board[0].length;s++) {
+      board[r+s][c+s] += shift;
+    }
+    for(int s = 1;r+s >= 0 && r+s < board.length && c+s >= 0 && c+s < board[0].length;s++) {
+      board[r-s][c+s] += shift;
+    }
+    for(int s = 1;c+s >= 0 && c+s < board[0].length;s++) {
+      board[r][c+s] += shift;
+    }
+  }
 
 
 
